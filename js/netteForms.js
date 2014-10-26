b4f334b66be6b5dbfd28ab75e4ac7cac3039e56c
David Grudl <david@grudl.com>
2014-10-26 15:34:55 +0100

    update

--- js/netteForms.js
+++ js/netteForms.js
@@ -61,8 +61,11 @@ Nette.getValue = function(elem) {
 		}
 		return values;
 
-	} else if (elem.type in {checkbox: 1, radio: 1}) {
+	} else if (elem.type === 'checkbox') {
 		return elem.checked;
+
+	} else if (elem.type === 'radio') {
+		return elem.checked && elem.value;
 
 	} else if (elem.type === 'file') {
 		return elem.files || elem.value;
@@ -195,11 +198,11 @@ Nette.isDisabled = function(elem) {
  * Display error message.
  */
 Nette.addError = function(elem, message) {
-	if (elem.focus) {
-		elem.focus();
-	}
 	if (message) {
 		alert(message);
+	}
+	if (elem.focus) {
+		elem.focus();
 	}
 };
 
@@ -418,7 +421,7 @@ Nette.toggleControl = function(elem, rules, topSuccess, firsttime) {
 			}
 			for (var id2 in rule.toggle || []) {
 				if (__hasProp.call(rule.toggle, id2)) {
-					Nette.toggles[id2] = Nette.toggles[id2] || (success ^ !rule.toggle[id2]);
+					Nette.toggles[id2] = Nette.toggles[id2] || (rule.toggle[id2] ? success : !success);
 				}
 			}
 		}
